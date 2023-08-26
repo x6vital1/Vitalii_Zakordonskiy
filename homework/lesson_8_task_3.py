775afe866417413427da80b8264a226a9ef2ef61
x6vital1 <x6vital@gmail.com>
2023-08-26 16:45:04 +0300

    lesson 8 task 1-3

---
+++ homework/lesson_8_task_3.py
@@ -0,0 +1,19 @@
+import random
+
+pair_numbers = 0
+unpaired_numbers = 0
+random_numbers = [random.randint(1, 10) for i in range(15)]
+
+for num in random_numbers:
+    if num % 2 == 0:
+        pair_numbers += num
+    else:
+        unpaired_numbers += num
+
+print(random_numbers)
+
+if pair_numbers < unpaired_numbers:
+    print("Yes")
+else:
+    print("No")
+60
