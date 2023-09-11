adb126048878c65454c36be5cf336c47372dfed4
x6vital1 <x6vital@gmail.com>
2023-09-11 20:21:50 +0300

    lesson 13_update

--- homework/lesson_13/lesson_13_task_2.py
+++ homework/lesson_13/lesson_13_task_2.py
@@ -1,16 +1,9 @@
 def dict_handler(link_on_dict, key, default_value):
-    """
-
-    :param link_on_dict: .....
-    :param key: .....
-    :param default_value:.....
-    :return:
-    """
+    value = default_value
     try:
         value = link_on_dict[key]
     except KeyError:
         link_on_dict[key] = default_value
-        value = default_value
     except TypeError:
         raise ValueError("Ключ не может быть переменным типом данных")
     finally:
@@ -19,6 +12,6 @@ def dict_handler(link_on_dict, key, default_value):
 
 # Приклад використання функції
 my_dict = {'a': 1, 'b': 2}
-result = dict_handler(my_dict, 'c', 4)
+result = dict_handler(my_dict, [2,2,2,2], 4)
 print(f"Новый словарь: {my_dict}")
 print(f"Резултатат: {result}")
